eee07ccaeb10a8e81eaffda4098a5b45db8074a6
“Jewelle98” <1502707a@student.tp.edu.sg>
2017-07-30 23:27:54 +0800

    6.2.1.6 to 6.2.1.13

--- Verification.sh
+++ Verification.sh
@@ -152,8 +152,289 @@ else
 	fi
 fi
 
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.6
+
+echo -e "\e[4m6.2.1.6 : Record Events That Modify Date and Time Information\e[0m\n"
+
+checksystem=`uname -m | grep "64"`
+checkmodifydatetimeadjtimex=`egrep 'adjtimex|settimeofday|clock_settime' /etc/audit/audit.rules`
+
+if [ -z "$checksystem" ]
+then
+	echo "It is a 32-bit system."
+	printf "\n"
+
+	if [ -z "$checkmodifydatetimeadjtimex" ]
+	then
+		echo "Date & Time Modified Events - FAILED (Events where system date and/or time has been modified are not captured)"
+
+	else
+		echo "Date & Time Modified Events - PASSED (Events where system date and/or time has been modified are captured)"
+	fi
+
+else
+	echo "It is a 64-bit system."
+	printf "\n" 
+
+	if [ -z "$checkmodifydatetimeadjtimex" ]
+	then
+		echo "Date & Time Modified Events - FAILED (Events where system date and/or time has been modified are not captured)"
+
+	else
+		echo "Date & Time Modified Events - PASSED (Events where system date and/or time has been modified are captured)"
+	fi
+fi
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.7
+
+echo -e "\e[4m6.2.1.7 : Record Events That Modify User/Group Information\e[0m\n"
+
+checkmodifyusergroupinfo=`egrep '\/etc\/group' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyusergroupinfo" ]
+then
+	echo "Group Configuration - FAILED (Group is not configured)"
+
+else
+	echo "Group Configuration - PASSED (Group is already configured)"
+fi
+
+printf "\n"
+
+checkmodifyuserpasswdinfo=`egrep '\/etc\/passwd' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyuserpasswdinfo" ]
+then
+	echo "Password Configuration - FAILED (Password is not configured)"
+
+else
+	echo "Password Configuration - PASSED (Password is configured)"
+fi
+
+printf "\n"
+
+checkmodifyusergshadowinfo=`egrep '\/etc\/gshadow' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyusergshadowinfo" ]
+then
+	echo "GShadow Configuration - FAILED (GShadow is not configured)"
+
+else
+	echo "GShadow Configuration - PASSED (GShadow is configured)"
+fi
+
+printf "\n"
+
+checkmodifyusershadowinfo=`egrep '\/etc\/shadow' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyusershadowinfo" ]
+then
+	echo "Shadow Configuration - FAILED (Shadow is not configured)"
+
+else
+	echo "Shadow Configuration - PASSED (Shadow is configured)"
+fi
+
+printf "\n"
+
+checkmodifyuseropasswdinfo=`egrep '\/etc\/security\/opasswd' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyuseropasswdinfo" ]
+then
+	echo "OPasswd Configuration- FAILED (OPassword not configured)"
+
+else
+	echo "OPasswd Configuration - PASSED (OPassword is configured)"
+fi
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.8
+
+echo -e "\e[4m6.2.1.8 : Record Events That Modify the System's Network Environment\e[0m\n"
+
+checksystem=`uname -m | grep "64"`
+checkmodifynetworkenvironmentname=`egrep 'sethostname|setdomainname' /etc/audit/audit.rules`
+
+if [ -z "$checksystem" ]
+then
+	echo "It is a 32-bit system."
+	printf "\n"
+	
+	if [ -z "$checkmodifynetworkenvironmentname" ]
+	then
+		echo "Modify the System's Network Environment Events - FAILED (Sethostname and setdomainname is not configured)"
+
+	else
+		echo "Modify the System's Network Environment Events - PASSED (Sethostname and setdomainname is configured)"
+	fi
+
+else
+	echo "It is a 64-bit system."
+	printf "\n"
+	
+	if [ -z "$checkmodifynetworkenvironmentname" ]
+	then
+        echo "Modify the System's Network Environment Events - FAILED (Sethostname and setdomainname is not configured)"
+
+	else
+		echo "Modify the System's Network Environment Events - PASSED (Sethostname and setdomainname is configured)"
+	fi
+fi
+
+printf "\n"
+
+checkmodifynetworkenvironmentissue=`egrep '\/etc\/issue' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifynetworkenvironmentissue" ]
+then
+    echo "Modify the System's Network Environment Events - FAILED (/etc/issue is not configured)"
+
+else
+    echo "Modify the System's Network Environment Events - PASSED (/etc/issue is configured)"
+fi
+
+printf "\n"
+
+checkmodifynetworkenvironmenthosts=`egrep '\/etc\/hosts' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifynetworkenvironmenthosts" ]
+then
+    echo "Modify the System's Network Environment Events - FAILED (/etc/hosts is not configured)"
+
+else
+	echo "Modify the System's Network Environment Events - PASSED (/etc/hosts is configured)"
+fi
+
+printf "\n"
+
+checkmodifynetworkenvironmentnetwork=`egrep '\/etc\/sysconfig\/network' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifynetworkenvironmentnetwork" ]
+then
+    echo "Modify the System's Network Environment Events - FAILED (/etc/sysconfig/network is not configured)"
+
+else
+    echo "Modify the System's Network Environment Events - PASSED (/etc/sysconfig/network is configured)"
+fi
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.9
+
+echo -e "\e[4m6.2.1.9 : Record Events That Modify the System's Mandatory Access Controls\e[0m\n"
+
+checkmodifymandatoryaccesscontrol=`grep \/etc\/selinux /etc/audit/audit.rules`
+
+if [ -z "$checkmodifymandatoryaccesscontrol" ]
+then
+	echo "Modify the System's Mandatory Access Controls Events - FAILED (Recording of modified system's mandatory access controls events is not configured)"
+
+else
+	echo "Modify the System's Mandatory Access Controls Events - PASSED (Recording of modified system's mandatory access controls events is configured)"
+fi
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.10
+
+echo -e "\e[4m6.2.1.10 : Collect Login and Logout Events\e[0m\n"
+
+chklogins=`grep logins /etc/audit/audit.rules`
+loginfail=`grep "\-w /var/log/faillog -p wa -k logins" /etc/audit/audit.rules`
+loginlast=`grep "\-w /var/log/lastlog -p wa -k logins" /etc/audit/audit.rules`
+logintally=`grep "\-w /var/log/tallylog -p wa -k logins" /etc/audit/audit.rules`
+
+if [ -z "$loginfail" -o -z "$loginlast" -o -z "$logintally" ]
+then
+	echo "FAILED - Login and logout events not recorded."
+
+else
+	echo "PASSED - Login and logout events recorded."
+fi
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.11
+
+echo -e "\e[4m6.2.1.11 : Collect Session Initiation Information\e[0m\n"
+
+chksession=`egrep 'wtmp|btmp|utmp' /etc/audit/audit.rules`
+sessionwtmp=`egrep "\-w /var/log/wtmp -p wa -k session" /etc/audit/audit.rules`
+sessionbtmp=`egrep "\-w /var/log/btmp -p wa -k session" /etc/audit/audit.rules`
+sessionutmp=`egrep "\-w /var/run/utmp -p wa -k session" /etc/audit/audit.rules`
+
+if [ -z "$sessionwtmp" -o -z "$sessionbtmp" -o -z "sessionutmp" ]
+then
+	echo "FAILED - Session initiation information not collected."
+
+else
+	echo "PASSED - Session initiation information is collected."
+fi
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.12
+
+echo -e "\e[4m6.2.1.12 : Collect Discretionary Access Control Permission Modification Events\e[0m\n"
+
+chkpermission64=`grep perm_mod /etc/audit/audit.rules`
+permission1=`grep "\-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission2=`grep "\-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F
+auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission3=`grep "\-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission4=`grep "\-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission5=`grep "\-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -Fauid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission6=`grep "\-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S
+ fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F
+auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+if [ -z "$permission1" -o -z "$permission2" -o -z permission3 -o -z permission4 -o -z permission5 -o -z permission6 ]
+then
+	echo "FAILED - Permission modifications not recorded."
+
+else
+	echo "PASSED - Permission modification are recorded."
+fi
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.13
+
+echo -e "\e[4m6.2.1.13 : Collect Unsuccessful Unauthorized Access Attempts to Files\e[0m\n"
+
+chkaccess=`grep access /etc/audit/audit.rules`
+access1=`grep "\-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access2=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access3=`grep "\-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access4=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access5=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access6=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+if [ -z "$access1" -o -z "$access2" -o -z "$access3" -o -z "$access4" -o -z "$access5" -o -z "$access6" ]
+then
+	echo "FAILED - Unsuccesful attempts to access files."
+
+else
+	echo "PASSED - Successful attempts to access files."
+fi
+
+printf "\n\n"
+
 ##########################################################################################
 
-read -n 1 -s -r -p "Press any key to exit!"
-kill -9 $PPID
+#read -n 1 -s -r -p "Press any key to exit!"
+#kill -9 $PPID
 
