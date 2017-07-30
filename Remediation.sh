b271c86cb76a7acf21f2d4cbbe34cef09000b174
“Jewelle98” <1502707a@student.tp.edu.sg>
2017-07-30 23:43:02 +0800

    6.2.1.6 to 6.2.1.13

--- Remediation.sh
+++ Remediation.sh
@@ -238,6 +238,440 @@ else
 	fi
 fi
 
+printf "\n\n"
+
+##########################################################################################
+#start of 6.2.1.6 coding
+
+echo -e "\e[4m6.2.1.6 : Record Events That Modify Date and Time Information\e[0m\n"
+checksystem=`uname -m | grep "64"`
+checkmodifydatetimeadjtimex=`egrep 'adjtimex|settimeofday|clock_settime' /etc/audit/audit.rules`
+
+if [ -z "$checksystem" ]
+then
+	echo "It is a 32-bit system."
+	printf "\n"
+	
+	if [ -z "$checkmodifydatetimeadjtimex" ]
+	then
+        echo "Date & Time Modified Events : FAILED (Events where system date and/or time has been modified are not captured)"
+        echo "-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change" >> /etc/audit/audit.rules
+		echo "-a always,exit -F arch=b32 -S clock_settime -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b32 -S clock_settime -k time-change" >> /etc/audit/audit.rules
+		echo "-w /etc/localtime -p wa -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-w /etc/localtime -p wa -k time-change" >> /etc/audit/audit.rules
+        echo "Events where system date and/or time has been modified will now be captured"
+
+	else
+		echo "Date & Time Modified Events : PASSED (Events where system date and/or time has been modified are captured)"
+	fi
+
+else
+	echo "It is a 64-bit system."
+	printf "\n"
+	
+	if [ -z "$checkmodifydatetimeadjtimex" ]
+	then
+        echo "Date & Time Modified Events : FAILED (Events where system date and/or time has been modified are not captured)"
+		echo "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change" >> /etc/audit/audit.rules
+        echo "-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change" >> /etc/audit/audit.rules
+		echo "-a always,exit -F arch=b64 -S clock_settime -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b64 -S clock_settime -k time-change" >> /etc/audit/audit.rules
+        echo "-a always,exit -F arch=b32 -S clock_settime -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b32 -S clock_settime -k time-change" >> /etc/audit/audit.rules
+		echo "-w /etc/localtime -p wa -k time-change" >> /etc/audit/rules.d/audit.rules
+		echo "-w /etc/localtime -p wa -k time-change" >> /etc/audit/audit.rules
+        echo "Events where system date and/or time has been modified will now be captured"
+
+	else
+		echo "Date & Time Modified Events : PASSED (Events where system date and/or time has been modified are captured)"
+	fi
+fi
+
+pkill -P 1 -HUP auditd
+#End of 6.2.1.6 coding
+
+printf "\n\n"
+
+##########################################################################################
+#Start of 6.1.2.7 coding
+
+echo -e "\e[4m6.2.1.7 : Record Events That Modify User/Group Information\e[0m\n"
+checkmodifyusergroupinfo=`egrep '\/etc\/group' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyusergroupinfo" ]
+then
+	echo "Group Configuration - FAILED (Group is not configured)"
+	echo "-w /etc/group -p wa -k identity" >> /etc/audit/audit.rules
+	echo "-w /etc/group -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+	echo "Group will now be configured"
+
+else
+	echo "Group Configuration - PASSED (Group is already configured)"
+fi
+
+printf "\n"
+
+checkmodifyuserpasswdinfo=`egrep '\/etc\/passwd' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyuserpasswdinfo" ]
+then
+	echo "Password Configuration - FAILED (Password is not configured)"
+	echo "-w /etc/passwd -p wa -k identity" >> /etc/audit/audit.rules
+	echo "-w /etc/passwd -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+	echo "Password will now be configured"
+
+else
+	echo "Password Configuration - PASSED (Password is configured)"
+fi
+
+printf "\n"
+
+checkmodifyusergshadowinfo=`egrep '\/etc\/gshadow' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyusergshadowinfo" ]
+then
+	echo "GShadow Configuration - FAILED (GShadow is not configured)"
+	echo "-w /etc/gshadow -p wa -k identity" >> /etc/audit/audit.rules
+	echo "-w /etc/gshadow -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+	echo "GShadow will now be configured"
+
+else
+	echo "GShadow Configuration - PASSED (GShadow is configured)"
+fi
+
+printf "\n"
+
+checkmodifyusershadowinfo=`egrep '\/etc\/shadow' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyusershadowinfo" ]
+then
+	echo "Shadow Configuration - FAILED (Shadow is not configured)"
+	echo "-w /etc/shadow -p -k identity" >> /etc/audit/audit.rules
+	echo "-w /etc/shadow -p -k identity" >> /etc/audit/rules.d/audit.rules
+	echo "Shadow will now be configured"
+	
+else
+	echo "SHadow Configuration - PASSED (Shadow is configured)"
+fi
+
+printf "\n"
+
+checkmodifyuseropasswdinfo=`egrep '\/etc\/security\/opasswd' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifyuseropasswdinfo" ]
+then
+	echo "OPasswd Configuration- FAILED (OPassword not configured)"
+	echo "-w /etc/security/opasswd -p wa -k identity" >> /etc/audit/audit.rules
+	echo "-w /etc/security/opasswd -p wa -k identity" >> /etc/audit/rules.d/audit.rules
+	echo "OPassword will now be configured"
+
+else
+	echo "OPasswd Configuration - PASSED (OPassword is configured)"
+fi
+
+pkill -P 1 -HUP auditd
+#End of 6.2.1.7 coding
+
+printf "\n\n"
+
+##########################################################################################
+#Start of 6.2.1.8 coding
+
+echo -e "\e[4m6.2.1.8 : Record Events That Modify the System's Network Environment\e[0m\n"
+checksystem=`uname -m | grep "64"`
+checkmodifynetworkenvironmentname=`egrep 'sethostname|setdomainname' /etc/audit/audit.rules`
+
+if [ -z "$checksystem" ]
+then
+	echo "It is a 32-bit system."
+	printf "\n"
+	
+	if [ -z "$checkmodifynetworkenvironmentname" ]
+	then
+        echo "Modify the System's Network Environment Events : FAILED (Sethostname and setdomainname is not configured)"
+        echo "-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/audit.rules
+        echo "Sethostname and setdomainname will now be configured"
+
+	else
+		echo "Modify the System's Network Environment Events : PASSED (Sethostname and setdomainname is configured)"
+	fi
+
+else
+	echo "It is a 64-bit system."
+	printf "\n"
+	
+	if [ -z "$checkmodifynetworkenvironmentname" ]
+	then
+        echo "Modify the System's Network Environment Events : FAILED (Sethostname and setdomainname is not configured)"
+        echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/audit.rules
+		echo "-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/rules.d/audit.rules
+		echo "-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale" >> /etc/audit/audit.rules
+        echo "Sethostname will now be configured"
+
+	else
+		echo "Modify the System's Network Environment Events : PASSED (Sethostname and setdomainname is configured)"
+	fi
+fi
+
+printf "\n"
+
+checkmodifynetworkenvironmentissue=`egrep '\/etc\/issue' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifynetworkenvironmentissue" ]
+then
+	echo "Modify the System's Network Environment Events : FAILED (/etc/issue is not configured)"
+	echo "-w /etc/issue -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+	echo "-w /etc/issue -p wa -k system-locale" >> /etc/audit/audit.rules
+	echo "-w /etc/issue.net -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+	echo "-w /etc/issue.net -p wa -k system-locale" >> /etc/audit/audit.rules
+	echo "/etc/issue will now be configured"
+
+else
+	echo "Modify the System's Network Environment Events : PASSED (/etc/issue is configured)"
+fi
+
+printf "\n"
+
+checkmodifynetworkenvironmenthosts=`egrep '\/etc\/hosts' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifynetworkenvironmenthosts" ]
+then
+	echo "Modify the System's Network Environment Events : FAILED (/etc/hosts is not configured)"
+	echo "-w /etc/hosts -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+	echo "-w /etc/hosts -p wa -k system-locale" >> /etc/audit/audit.rules
+	echo "/etc/hosts will now be configured"
+
+else
+	echo "Modify the System's Network Environment Events : PASSED (/etc/hosts is configured)"
+fi
+
+printf "\n"
+
+checkmodifynetworkenvironmentnetwork=`egrep '\/etc\/sysconfig\/network' /etc/audit/audit.rules`
+
+if [ -z "$checkmodifynetworkenvironmentnetwork" ]
+then
+	echo "Modify the System's Network Environment Events : FAILED (/etc/sysconfig/network is not configured)"
+	echo "-w /etc/sysconfig/network -p wa -k system-locale" >> /etc/audit/rules.d/audit.rules
+	echo "-w /etc/sysconfig/network -p wa -k system-locale" >> /etc/audit/audit.rules
+	echo "/etc/sysconfig/network will now be configured"
+
+else
+	echo "Modify the System's Network Environment Events : PASSED (/etc/sysconfig/network is configured)"
+fi
+
+pkill -P 1 -HUP auditd
+#End of 6.2.1.8 coding
+
+printf "\n\n"
+
+##########################################################################################
+#Start of 6.1.2.9 coding
+
+echo -e "\e[4m6.2.1.9 : Record Events That Modify the System's Mandatory Access Controls\e[0m\n"
+var=$(grep \/etc\/selinux /etc/audit/audit.rules)
+
+if [ -z "$var" ]
+then
+	printf "Monitoring SELinux Mandatory Access Controls : FAILED (/etc/selinux is not configured)\n"
+	printf "\n-w /etc/selinux/ -p wa -k MAC-policy" >> /etc/audit/audit.rules
+	printf "/etc/selinux will now be configured"
+	
+else
+	printf "Monitoring SELinux Mandatory Access Controls : PASSED (/etc/selinux is configured)\n"
+fi
+#End of 6.2.1.9 coding
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.10
+
+echo -e "\e[4m6.2.1.10 : Collect Login and Logout Events\e[0m\n"
+
+loginfail=`grep "\-w /var/log/faillog -p wa -k logins" /etc/audit/audit.rules`
+loginlast=`grep "\-w /var/log/lastlog -p wa -k logins" /etc/audit/audit.rules`
+logintally=`grep "\-w /var/log/tallylog -p wa -k logins" /etc/audit/audit.rules`
+
+if [ -z "$loginfail" -o -z "$loginlast" -o -z "$logintally" ]
+then
+	if [ -z "$loginfail" ]
+	then
+		echo "-w /var/log/faillog -p wa -k logins" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$loginlast" ]
+	then
+		echo "-w /var/log/lastlog -p wa -k logins" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$logintally" ]
+	then
+		echo "-w /var/log/tallylog -p wa -k logins" >> /etc/audit/audit.rules
+	fi
+	
+	echo "Login and Logout Events collected"
+fi
+	
+pkill -P 1 -HUP auditd
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.11
+
+echo -e "\e[4m6.2.1.11 : Collect Session Initiation Information\e[0m\n"
+
+sessionwtmp=`egrep '\-w /var/log/wtmp -p wa -k session' /etc/audit/audit.rules`
+sessionbtmp=`egrep '\-w /var/log/btmp -p wa -k session' /etc/audit/audit.rules`
+sessionutmp=`egrep '\-w /var/run/utmp -p wa -k session' /etc/audit/audit.rules`
+
+if [ -z "$sessionwtmp" -o -z "$sessionbtmp" -o -z "$sessionutmp" ]
+then 
+	if [ -z "$sessionwtmp"]
+	then 
+		echo "-w /var/log/wtmp -p wa -k session" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$sessionbtmp"]
+	then 
+		echo "-w /var/log/btmp -p wa -k session" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$sessionutmp"]
+	then
+		echo "-w /var/run/utmp -p wa -k session" >> /etc/audit/audit.rules
+	fi
+	
+	echo "Session Initiation Information Collected"
+fi
+
+pkill -HUP -P 1 auditd
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.12
+
+echo -e "\e[4m6.2.1.12 : Collect Discretionary Access Control Permission Modification Events\e[0m\n"
+
+permission1=`grep "\-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission2=`grep "\-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission3=`grep "\-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission4=`grep "\-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission5=`grep "\-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -Fauid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission6=`grep "\-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+if [ -z "$permission1" -o -z "$permission2" -o -z permission3 -o -z permission4 -o -z permission5 -o -z permission6  ]
+then 
+	if [ -z "$permission1" ]
+	then
+		echo "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+
+	if [ -z "$permission2" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$permission3" ]
+	then 
+		echo "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$permission4" ]
+	then
+		echo "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$permission5" ]
+	then 
+		echo "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$permission6" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	
+	echo "Discretionary Access Control Permission Modification Events Collected"
+fi
+
+pkill -P 1 -HUP auditd
+
+printf "\n\n"
+
+##########################################################################################
+#6.2.1.13
+
+echo -e "\e[4m6.2.1.13 : Collect Unsuccessful Unauthorized Access Attempts to Files\e[0m\n"
+
+access1=`grep "\-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access2=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access3=`grep "\-a always,exit -F arch=b64 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access4=`grep "\-a always,exit -F arch=b32 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access5=`grep "\-a always,exit -F arch=b32 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access6=`grep "\-a always,exit -F arch=b32 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+if [ -z "$access1" -o -z "$access2" ]
+then
+	if [ -z "$access1" ]
+	then     
+   		echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$access2" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" >> /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$access3" ]
+	then
+		echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$access4" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$access5" ]
+	then
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+	
+	if [ -z "$access6" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+	
+	echo "Unsuccessful Unauthorized Access Attempts to Files Collected"
+fi
+
+pkill -P 1 -HUP auditd
+
+printf "\n\n"
+
 ##########################################################################################
 
 #read -n 1 -s -r -p "Press any key to exit!"
